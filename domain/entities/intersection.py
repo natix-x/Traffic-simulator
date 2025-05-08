3f7640d7126c10422431b53d73776d2b561411bf
Magda <wilkmagda@student.agh.edu.pl>
2025-05-08 18:36:22 +0200

    images and simple simulation

--- domain/entities/intersection.py
+++ domain/entities/intersection.py
@@ -1,25 +1,25 @@
 import uuid
 from dataclasses import dataclass, field
 
-from domain.entities import Vehicle, vehicle
+# from domain.entities import Vehicle, vehicle
 from domain.models import Position, VehicleState
 
 
 @dataclass
 class Intersection:
     id: uuid.UUID = field(default_factory=uuid.uuid4)
-    waiting_vehicles: dict[Position, list[Vehicle]] = field(default_factory=lambda: {d: [] for d in Position})
-
-    def add_vehicle(self, vehicle: Vehicle):
-        self.waiting_vehicles[vehicle.current_position].append(vehicle)
-
-    def get_next_vehicle(self, position: Position) -> Vehicle | None:
-        if self.waiting_vehicles[position]:
-            return self.waiting_vehicles[position][0]
-        return None
-
-    def move_vehicle_into_intersection(self, position: Position):
-        vehicle = self.get_next_vehicle(position)
-        if vehicle:
-            self.waiting_vehicles[position].pop(0)
-            vehicle.current_state = VehicleState.IN_INTERSECTION
+    # waiting_vehicles: dict[Position, list[Vehicle]] = field(default_factory=lambda: {d: [] for d in Position})
+    #
+    # def add_vehicle(self, vehicle: Vehicle):
+    #     self.waiting_vehicles[vehicle.current_position].append(vehicle)
+    #
+    # def get_next_vehicle(self, position: Position) -> Vehicle | None:
+    #     if self.waiting_vehicles[position]:
+    #         return self.waiting_vehicles[position][0]
+    #     return None
+    #
+    # def move_vehicle_into_intersection(self, position: Position):
+    #     vehicle = self.get_next_vehicle(position)
+    #     if vehicle:
+    #         self.waiting_vehicles[position].pop(0)
+    #         vehicle.current_state = VehicleState.IN_INTERSECTION
--- ui/app.py
+++ ui/app.py
@@ -3,6 +3,8 @@ import os
 import pygame
 from pygame.locals import *
 
+from domain.entities import TrafficLight, Intersection
+from domain.models import Position, TrafficLightState
 from ui.buttons.start_game_button import StartGameButton
 from config import AppConfig
 
@@ -17,16 +19,63 @@ class App:
         self.background = pygame.transform.scale(
             pygame.image.load(os.path.join("ui", "resources", "images", "intersection.png")), (512, 512)
         )
+        self.vehicle = pygame.transform.scale(
+            pygame.image.load(os.path.join("ui", "resources", "images", "cars", "car_E.png")), (40, 25)
+        )
         self._initialize_display()
 
     def _start_simulation(self):
-        pygame.display.set_caption("Simulation")
+        clock = pygame.time.Clock()
+        dt = 0
+        start_pos = pygame.Vector2(0, 296)
+        intersection = Intersection()
+        light = TrafficLight(state=TrafficLightState.RED, intersection=intersection, position=Position.W)
+        light_time = 0
+
+        # taffic_system = TrafficSystem()
+        # traffic_system.add_traffic_light(light)
+        # traffic_system.add_in
+
         while self.running:
             self.screen.blit(self.background, (0, 0))
             for event in pygame.event.get():
                 if event.type == QUIT:
                     self.running = False
-            pygame.display.update()
+            # pygame.display.update() # jak to jest to bardzo miga
+
+            pygame.display.set_caption("Simulation")
+
+            # lokalizacja świateł dla jednego skrzyżowania
+            pygame.draw.circle(self.screen, 'red', (146, 146), 5)
+            pygame.draw.circle(self.screen, 'red', (366, 146), 5)
+            pygame.draw.circle(self.screen, 'red', (146, 366), 5)
+            pygame.draw.circle(self.screen, 'red', (366, 366), 5)
+
+            # pozycje pojazdów
+            pygame.draw.circle(self.screen, 'blue', (216, 142), 20)
+            #pygame.draw.circle(self.screen, 'blue', (142, 296), 20)
+            pygame.draw.circle(self.screen, 'blue', (370, 216), 20)
+            pygame.draw.circle(self.screen, 'blue', (296, 370), 20)
+
+            # coś co działa
+            self.screen.blit(self.vehicle, start_pos)
+            pygame.display.flip()
+
+            # zmiana pozycji - nie działa najlepiej
+            if start_pos.x < 122 or (light.is_green() and start_pos.x >= 122):
+                dt = clock.tick(60) / 10 # wolna basic prędkość -> szybsze można mnożyć przez coś >1
+                start_pos.x = (start_pos.x + dt) % 512
+
+            light_time += 1 # uzależnić od czasu
+            if light_time % 500 == 0:
+                if light.is_green():
+                    light.change_state(TrafficLightState.RED)
+                    print("red")
+                else:
+                    light.change_state(TrafficLightState.GREEN)
+
+
+
         pygame.quit()
 
         # simulation_engine = SimulationEngine(TrafficSystem(), self.screen)  # jeśli wymaga ekranu
