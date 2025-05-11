e24a7bcbe176cb03d852dae3d1c856ab97a140fc
Magda <wilkmagda@student.agh.edu.pl>
2025-05-11 21:26:25 +0200

    Various types of vehicles on intersection

--- domain/entities/intersection.py
+++ domain/entities/intersection.py
@@ -1,19 +1,29 @@
 import uuid
 from dataclasses import dataclass, field
+from typing import TYPE_CHECKING
 
-from domain.entities import Vehicle, vehicle
 from domain.models import Position, VehicleState
+
+if TYPE_CHECKING:
+    from domain.entities.vehicle import Vehicle
 
 
 @dataclass
 class Intersection:
     id: uuid.UUID = field(default_factory=uuid.uuid4)
-    waiting_vehicles: dict[Position, list[Vehicle]] = field(default_factory=lambda: {d: [] for d in Position})
+    waiting_vehicles: dict[Position, list["Vehicle"]] = field(default_factory=lambda: {d: [] for d in Position})
 
-    def add_vehicle(self, vehicle: Vehicle):
+    # dynamicznie zmieniające sie linie stopu w zależności od ilości czekających pojazdów
+    stop_line_N = 90
+    stop_line_E = 364
+    stop_line_W = 116
+    stop_line_S = 394
+
+
+    def add_vehicle(self, vehicle: "Vehicle"):
         self.waiting_vehicles[vehicle.current_position].append(vehicle)
 
-    def get_next_vehicle(self, position: Position) -> Vehicle | None:
+    def get_next_vehicle(self, position: Position): # -> "Vehicle" | None:
         if self.waiting_vehicles[position]:
             return self.waiting_vehicles[position][0]
         return None
